d13d9f69b5e2c8a2b0ade312e2676fd9aa23b400
Gatuk S. Chattanon <gatukgl@gmail.com>
2020-05-27 16:09:55 +0700

    Initialize food tracker app :baby_chick:

---
+++ MyFoodTracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyFoodTracker
+//
+//  Created by Gatuk S. Chattanon on 27/5/2563 BE.
+//  Copyright © 2563 Gatuk S. Chattanon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
